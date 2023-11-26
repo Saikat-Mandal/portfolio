867615529de7e282437178394b4851f46de19731
Saikat Mandal <msaikat022@gmail.com>
2023-11-26 20:13:53 +0530

    made navbar responsive

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,11 +7,18 @@ function About() {
 
     return (
         <Home>
-            <div className="flex items-center  justify-around px-10">
-                <img className=" w-1/4" src="https://cdn.dribbble.com/users/31752/screenshots/1627371/media/b58991464a72d13ebec186f743e15045.png?resize=800x600&vertical=center" alt="" />
-                <div className=" w-2/4">
-                    <p className=" text-5xl">Hi there !<br /> I am Saikat Mandal</p>
-                    <p>I'm a developer, designer and linguist who has been building for the web in some capacity since 2001. I specialise in accessibility, performance and usability without sacrificing creativity.</p>
+            <div className="flex justify-center md:justify-end h-screen overflow-hidden ">
+                <div className="h-full p-10  lg:w-2/3 overflow-y-auto lg:mr-60 scrollbar-hide">
+                    <div className=" md:flex-col flex gap-x-4">
+                        <h1 className=" text-6xl md:text-8xl  lg:text-9xl  text-right">More</h1>
+                        <h1 className=" text-6xl md:text-8xl  lg:text-9xl  text-right">of</h1>
+                        <h1 className=" text-6xl md:text-8xl  lg:text-9xl  text-right">me</h1>
+                    </div>
+                    <p className=" py-14 ">As a full stack developer and student of engineering, I have a diverse range of skills and experiences that contribute to my unique personality. I am highly adaptable, able to work in both individual and team settings, and am always eager to learn and take on new challenges.<br /><br />
+                        In my work as a full stack developer, I have experience in a variety of programming languages and frameworks, including HTML, CSS, JavaScript, React, and Node.js. I am also experienced in database management and server-side scripting, allowing me to create fully-functional web applications from start to finish.<br /><br />
+                        As a student of engineering, I have developed strong problem-solving skills and a solid understanding of technical concepts. I am highly detail-oriented and able to think critically and logically, which allows me to tackle complex challenges with ease.<br /><br />
+                        Overall, my unique blend of technical and interpersonal skills allows me to work well in a variety of settings and contribute positively to any team or project.As </p>
+                    <div className=" h-40 w-40 "></div>
                 </div>
             </div>
 
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,26 +2,36 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useState } from 'react'
 import Switch from "react-switch";
+import { GiHamburgerMenu } from "react-icons/gi";
 import ContactModal from './modals/ContactModal';
 function Navbar() {
     const pathname = usePathname()
 
-    return (
-        <div className='flex p-12 items-center justify-between'>
-            {/* <Switch /> */}
-            <div className=' flex bg-[#EBEBEB] w-1/5 px-1 justify-between py-1 rounded-full '>
-                <div className={`py-1 px-3 rounded-full ${pathname == "/" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/">All</Link></div>
-                <div className={`py-1 px-3 rounded-full ${pathname == "/about" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/about">About</Link></div>
-                <div className={`py-1 px-3 rounded-full ${pathname == "/projects" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/projects">Projects</Link></div>
-                <div className={`py-1 px-3 rounded-full ${pathname == "/blogs" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/blogs">Blogs</Link></div>
-                {/* <div className={`py-1 px-3 rounded-full ${pathname == "/login" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/login">Login</Link></div>
-                <div className={`py-1 px-3 rounded-full ${pathname == "/register" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/signup">Register</Link></div> */}
-            </div>
+    const [showNavbar, setShowNavbar] = useState(true)
 
-            <div className=' px-3 py-1 border-2 border-dashed border-orange-500 rounded-full'><p className='typewriter' >Website still under development</p></div>
-            <button onClick={() => { setModalOpen(true) }} className=' px-3 py-1 border-4 hover:scale-105 border-orange-500 rounded-full'>Connect</button>
+
+    return (
+        <div className='flex lg:flex-row p-12 items-center justify-around  lg:justify-between gap-3 md:gap-0'>
+            {/* <Switch /> */}
+            {
+                showNavbar ? (
+                    <div className=' flex flex-col lg:flex-row bg-[#EBEBEB] w-2/5 lg:w-1/5 gap-8 lg:gap-0 p-8 lg:px-1 lg:justify-between lg:py-1 rounded-lg lg:rounded-full absolute lg:static self-end items-center z-20 top-[15%] right-0'>
+                        <div className={`py-1 px-3 rounded-full ${pathname == "/" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/">All</Link></div>
+                        <div className={`py-1 px-3 rounded-full ${pathname == "/about" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/about">About</Link></div>
+                        <div className={`py-1 px-3 rounded-full ${pathname == "/projects" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/projects">Projects</Link></div>
+                        <div className={`py-1 px-3 rounded-full ${pathname == "/blogs" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/blogs">Blogs</Link></div>
+                        {/* <div className={`py-1 px-3 rounded-full ${pathname == "/login" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/login">Login</Link></div>
+                <div className={`py-1 px-3 round ed-full ${pathname == "/register" ? "bg-[#FFFFFF]" : ""}`} > <Link href="/signup">Register</Link></div> */}
+                    </div>
+                ) : (<></>)
+            }
+
+            < div className=' px-3 py-1 border-2 border-dashed border-orange-500 rounded-full' > <p className='typewriter' >Website still under development</p>
+            </div >
+            <button onClick={() => { setModalOpen(true) }} className=' px-3 py-1 border-4 hover:scale-105 border-orange-500 rounded-full '>Connect</button>
+            <span onClick={() => setShowNavbar(prev => !prev)} className=' lg:hidden block cursor-pointer text-2xl'> <GiHamburgerMenu /></span>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
