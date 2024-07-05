9b0e376b391515f1c46e9c8f56fc47a9ef1f08fc
Saikat Mandal <msaikat022@gmail.com>
2024-07-05 20:00:14 +0530

    hover content added

--- src/app/components/ProjectComponent.jsx
+++ src/app/components/ProjectComponent.jsx
@@ -24,8 +24,19 @@ function ProjectComponent(props) {
             style={style}
         >
             <p></p>
-            <div className="hover-overlay absolute inset-0 bg-black bg-opacity-90 opacity-0 transition-opacity duration-300 flex items-center justify-center text-white text-3xl z-20">
-                Hover Content
+            <div className="hover-overlay absolute inset-0 bg-black bg-opacity-90 opacity-0 transition-opacity duration-300 flex flex-col text-white p-8 z-20">
+
+                <p className=' pb-2'><span className=' text-red-500'>Tech stack:</span> MongoDb , Express, React, Node </p>
+                <p className=' pb-2'> <span className=' text-red-500'>Description:</span> A social media platform for the commmunity.</p>
+                <p className=' pb-2'><span className=' text-red-500'>Learnings:</span> Acquired React skills, learnt components
+                    organization, effectively utilized multiple hooks, and refined
+                    API call orchestration.</p>
+                <div className='flex gap-x-3' >
+                    <span className=' text-red-500'>Links:</span>
+                    <Link href="/" className=' underline'>Live</Link>
+                    <Link href="/" className=' underline'>Code</Link>
+                </div>
+
             </div>
             <div><p className="text-3xl text-center text-white z-20">{props.title}</p></div>
 
